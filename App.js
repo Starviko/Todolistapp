f278e117b3b2ad16b4b1425a4458774a3230ea88
StarWalker-v <pengjingguo2022@gmail.com>
2023-09-28 18:15:29 -0600

    create new app

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput ,Image, Button } from 'react-native';
+ 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <View style={{width:350,height:60,backgroundColor: 'white', justifyContent:'center'}}>
+        <Text style={{fontSize: 20,fontWeight: 'bold',color: 'black',textAlign: 'center'}}>Lovely Deer</Text>
+      </View>
+      <Image style={styles.image}source={require('./assets/download.png')}></Image>      
+      <Text style={styles.red}>Hello World</Text>
+      <View>
+      <TextInput style={{height: 40, borderColor: 'gray', 
+       marginBottom: 15,borderWidth: 5, width: 250,backgroundColor:'white'}}
+       placeholder='Email'
+       autoCapitalize='none'></TextInput>
+      <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 5, width: 250,backgroundColor:'white'}}
+       placeholder='Username'
+       autoCapitalize='none'></TextInput>
+      </View>
       <StatusBar style="auto" />
+    
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ee82ee',
+    alignItems:'flex-start',
+    justifyContent:'space-around',
+    flexDirection:'column',
   },
+  bigblue: {
+    color: '#ff00ff',
+    fontWeight: 'bold',
+    fontsize: 20,
+  },
+  red: {
+    color: '#0000cd',
+    fontWeight: 'bold',
+    alignItems: 'center',
+    fontSize: 70,
+  },
+  image:
+  {
+  width: 350,
+  height: 350,
+  borderRadius: 18,
+  }
 });
